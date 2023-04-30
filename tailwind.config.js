629e733820a7bdb8ce2e64720f533d520aaad593
Kevin <kevdaviman.23@gmail.com>
2023-04-29 20:51:14 -0500

    web background image added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: theme => ({
+                'body-pattern': "url('../img/pattern.png')"
+            })
+        }
+    },
+    variants: {
+
+    }
+}
